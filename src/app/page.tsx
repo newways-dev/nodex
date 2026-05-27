02c1ac4a8944a602606aac560770e6e024b74e83
Kate <kate.dew.next@gmail.com>
2026-05-27 22:41:11 +0200

    setup inngest background job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,32 @@
-import { requireAuth } from '@/lib/auth-utils'
-import { caller } from '@/trpc/server'
+'use client'
 
-const Page = async () => {
-  await requireAuth()
+import { Button } from '@/components/ui/button'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { useTRPC } from '@/trpc/client'
+import { toast } from 'sonner'
 
-  const data = await caller.getUsers()
+const Page = () => {
+  const trpc = useTRPC()
+  const { data } = useQuery(trpc.getWorkflows.queryOptions())
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success('Job queued')
+      },
+      onError: (err) => {
+        toast.error(err.message)
+      },
+    }),
+  )
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server component
       <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
     </div>
   )
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,19 @@
+import { inngest } from '@/inngest/client'
 import { createTRPCRouter, protectedProcedure } from '../init'
 import prisma from '@/lib/db'
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany()
+  getWorkflows: protectedProcedure.query(() => {
+    return prisma.workflow.findMany()
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    const result = await inngest.send({
+      name: 'test/hello.world',
+      data: {
+        email: 'kate@gmail.com',
+      },
+    })
+
+    return { success: true, message: 'Job queued', result }
   }),
 })
 // export type definition of API
